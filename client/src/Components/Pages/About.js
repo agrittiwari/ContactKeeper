303e3bcc7b0f41cfa16402ceeb876a108a34b7c9
Agrit Tiwari <agrit1408@gmail.com>
2020-10-14 17:37:44 +0530

    CleanU, Navbar and Router Setup in Frontend

---
+++ client/src/Components/Pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About THis App</h1>
+      <p className="my-1">This is full Stack react app for keeping contacts</p>
+      <p className="bg-dark p">
+        <strong>Version:</strong> 1.1.0
+      </p>
+    </div>
+  );
+};
+export default About;

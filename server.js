06d9e90c7d20896ae6f42500a7d7dd8383615342
Agrit Tiwari <agrit1408@gmail.com>
2020-11-06 21:16:40 +0530

    Prepare for Deploy

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require('path');
+
 const app = express();
 
 //connect Database
@@ -17,6 +19,14 @@ app.get("/", (req, res) =>
 app.use("/api/users", require("./ROUTES/Users"));
 app.use("/api/auth", require("./ROUTES/Auth"));
 app.use("/api/contacts", require("./ROUTES/Contacts"));
+
+//serve static folder
+if(process.env.NODE_ENV === 'production'){
+  //set sttic folder
+  app.use(express.static('client/build'));
+  app.get('*',(req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
+
 
 const PORT = process.env.PORT || 5000;
 
